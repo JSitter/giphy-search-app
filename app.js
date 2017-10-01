61486e48c60e7b455754b05ffabfafde0eebb30c
Justin Sitter <justin@jaytria.com>
2017-10-01 16:36:36 -0700

    Added styling

--- app.js
+++ app.js
@@ -6,36 +6,18 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var http = require('http');
+var giphy = require('giphy-api')();
 
 //initialize express
 var app = express();
 
+//Tell Express static files will live in folder public
+app.use(express.static('public'));
+
 //setup home page
 app.get('/', function (req, res) {
-  console.log(req.query.term)
-  var queryString = req.query.term;
-  // ENCODE THE QUERY STRING TO REMOVE WHITE SPACES AND RESTRICTED CHARACTERS
-  var term = encodeURIComponent(queryString);
-  // PUT THE SEARCH TERM INTO THE GIPHY API SEARCH URL
-  var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC'
-
-  http.get(url, function(response) {
-    // SET ENCODING OF RESPONSE TO UTF8
-    response.setEncoding('utf8');
-
-    var body = '';
-
-    response.on('data', function(d) {
-      // CONTINUOUSLY UPDATE STREAM WITH DATA FROM GIPHY
-      body += d;
-    });
-
-    response.on('end', function() {
-      // WHEN DATA IS FULLY RECEIVED PARSE INTO JSON
-      var parsed = JSON.parse(body);
-      // RENDER THE HOME TEMPLATE AND PASS THE GIF DATA IN TO THE TEMPLATE
-      res.render('home', {gifs: parsed.data})
-    });
+  giphy.search(req.query.term, function (err, response) {
+    res.render('home', {gifs: response.data})
   });
 });
 
