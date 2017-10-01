7ee3e4254d8ee989992342345a9b384c49abe3ef
Justin Sitter <justin@jaytria.com>
2017-10-01 16:21:38 -0700

    conntect to giphy website

--- app.js
+++ app.js
@@ -5,9 +5,39 @@
  ***************************************************/
 var express = require('express');
 var exphbs  = require('express-handlebars');
+var http = require('http');
 
 //initialize express
 var app = express();
+
+//setup home page
+app.get('/', function (req, res) {
+  console.log(req.query.term)
+  var queryString = req.query.term;
+  // ENCODE THE QUERY STRING TO REMOVE WHITE SPACES AND RESTRICTED CHARACTERS
+  var term = encodeURIComponent(queryString);
+  // PUT THE SEARCH TERM INTO THE GIPHY API SEARCH URL
+  var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC'
+
+  http.get(url, function(response) {
+    // SET ENCODING OF RESPONSE TO UTF8
+    response.setEncoding('utf8');
+
+    var body = '';
+
+    response.on('data', function(d) {
+      // CONTINUOUSLY UPDATE STREAM WITH DATA FROM GIPHY
+      body += d;
+    });
+
+    response.on('end', function() {
+      // WHEN DATA IS FULLY RECEIVED PARSE INTO JSON
+      var parsed = JSON.parse(body);
+      // RENDER THE HOME TEMPLATE AND PASS THE GIF DATA IN TO THE TEMPLATE
+      res.render('home', {gifs: parsed.data})
+    });
+  });
+});
 
 //Setup handlebars to work with express
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -17,7 +47,17 @@ app.set('view engine', 'handlebars');
 app.get('/:username', function (req, res) {
   // Here you would look up the user from the database
   // Then render the template to display the users's info
-})
+});
+
+app.get('/hello-gif', function (req, res) {
+  var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
+  res.render('hello-gif', {gifUrl: gifUrl})
+});
+
+app.get('/greetings/:name', function (req, res) {
+  var name = req.params.name;
+  res.render('greetings', {name: name});
+});
 
 //listen on port 3001
 app.listen(3001, function () {
